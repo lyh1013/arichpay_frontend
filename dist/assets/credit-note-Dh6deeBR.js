02365592cdf52ee0438ea079f3dd0e85b0331ec2
evalin <eva.lin@ideaxpress.biz>
2025-03-14 16:25:12 +0800

    updates

---
+++ dist/assets/credit-note-Dh6deeBR.js
@@ -0,0 +1,1 @@
+const e=[{title:"操作",key:"actions",sortable:!1,align:"center"},{title:"日期",key:"date"},{title:"發票號碼",key:"invoice_number"},{title:"產品名稱",key:"name",sortable:!1},{title:"數量",key:"count"},{title:"單價",key:"price"},{title:"金額",key:"total"},{title:"營業稅額",key:"tax"},{title:"課稅別",key:"tax_type"}];export{e as h};
---
+++ dist/assets/logo-m-PyG3uc.js
@@ -0,0 +1,1 @@
+const s="/arichpay_web/assets/logo-zbwwUDvZ.png";export{s as _};

e4ed10410ed95a42b6a6f0938bebad5bb9872457
evalin <eva.lin@ideaxpress.biz>
2025-03-14 17:03:01 +0800

    debug i18n

---
+++ assets/ErrorView-BBDcyaek.js
@@ -0,0 +1,1 @@
+import{_ as e,g as r,o}from"./index-hSwMsLwQ.js";const c={};function n(t,s){return o(),r("div",null,"error view")}const a=e(c,[["render",n]]);export{a as default};
--- assets/core-Hb9pU66e.js
+++
@@ -1,1 +0,0 @@
-const o={sitename:"久裕 Pay",copyright:"© { year } Arich Enterprise Co., Ltd. All rights reserved.",cookiePolicy:"Cookie 政策",cookieContent:"本網站使用cookie。 繼續瀏覽本網站即表示您同意我們使用cookie。| 我們使用 cookies 收集有關用戶如何使用網站的信息，包括帳戶授權和防止本網站受到攻擊的安全措施，以提供更好的內容和體驗。如果不允許，網站功能可能無法正常運行。"};export{o as default};

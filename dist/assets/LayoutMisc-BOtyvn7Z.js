81627cec3bbaf553e3dff5ad3bd7b1dd6a4945a4
Eva <evalin1013@gmail.com>
2025-03-15 15:26:49 +0800

    debug i18n

--- dist/assets/LayoutMisc-BOtyvn7Z.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as o,w as e,a as t,f as s,r as c,e as r,V as l,o as _}from"./index-BmHhwhui.js";const u={};function f(i,d){const a=c("RouterView");return _(),o(l,{class:"h-screen d-flex align-center justify-center"},{default:e(()=>[t(r,null,{default:e(()=>[t(s,null,{default:e(()=>[t(a)]),_:1})]),_:1})]),_:1})}const p=n(u,[["render",f]]);export{p as default};
---
+++ dist/assets/LayoutMisc-CqRczPBC.js
@@ -0,0 +1,1 @@
+import{_ as n,c as o,w as e,a as t,f as s,r as c,e as r,V as l,o as _}from"./index-wje3kA3V.js";const u={};function f(i,d){const a=c("RouterView");return _(),o(l,{class:"h-screen d-flex align-center justify-center"},{default:e(()=>[t(r,null,{default:e(()=>[t(s,null,{default:e(()=>[t(a)]),_:1})]),_:1})]),_:1})}const p=n(u,[["render",f]]);export{p as default};
